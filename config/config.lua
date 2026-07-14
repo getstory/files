f6506ef0bd3456d0cc50997e1700cbf281d5d593
Story <antblastinmgmt@gmail.com>
2026-07-14 19:17:53 -0400

    Add files via upload

---
+++ config/config.lua
@@ -0,0 +1,41 @@
+shared.story = {
+
+	["Keybinds"] = {
+		["Combat"] = {
+			["Camlock"] = Enum.KeyCode.V
+		}
+	},
+
+
+	["Combat"] = {
+
+		["Camlock"] = {
+			["Enabled"] = true,
+
+			["Smoothing"] = 0.067,
+
+			["TargetPart"] = "Head",
+
+			["MaxDistance"] = 300,
+
+			["TeamCheck"] = true
+		}
+	},
+
+
+	["FOV"] = {
+
+		["Camlock"] = {
+
+			["Enabled"] = true,
+
+			["Radius"] = 150,
+
+			["Visible"] = true
+
+		}
+	}
+
+}
+
+return shared.story
