969e1854c4be4c8634c3864b49bb89f4bebabe12
Story <antblastinmgmt@gmail.com>
2026-07-15 17:38:20 -0400

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,41 +1,93 @@
-shared.story = {
+shared.Story = {
+    ['Settings'] = {
+        ['Target Aim'] = true,
+        ['Knock Check'] = true,
+        ['Visible Check'] = false,
+    },
 
-	["Keybinds"] = {
-		["Combat"] = {
-			["Camlock"] = Enum.KeyCode.V
-		}
-	},
+    ['Keybinds'] = {
+        ['Target Lock'] = {
+            ['Key'] = 'F',
+            ['Mode'] = 'Toggle',
+        },
+        ['Trigger Bot'] = {
+            ['Key'] = 'F',
+            ['Mode'] = 'Toggle',
+        },
+        ['Speed'] = 'Q',
+        ['ESP'] = 'E',
+    },
 
+    ['FOV'] = {
+        ['Enabled'] = false,
+        ['Visible'] = true,
+        ['Size'] = Vector2.new(250, 250),
+        ['Thickness'] = 2,
+        ['Color'] = Color3.fromRGB(255, 255, 255),
+    },
 
-	["Combat"] = {
+    ['Silent Aim'] = {
+        ['Enabled'] = true,
+        ['Hit Part'] = 'Closest Part',
+        ['Use Prediction'] = true,
+        ['Prediction'] = {
+            ['X'] = 0.133,
+            ['Y'] = 0.133,
+            ['Z'] = 0.133,
+        },
+    },
 
-		["Camlock"] = {
-			["Enabled"] = true,
+    ['Camera Lock'] = {
+        ['Enabled'] = true,
+        ['Hit Part'] = 'Closest Part',
+        ['Smoothing'] = 40,
+        ['Use Prediction'] = true,
+        ['Prediction'] = 0.133,
+    },
 
-			["Smoothing"] = 0.067,
+    ['Trigger Bot'] = {
+        ['Enabled'] = true,
+        ['Delay'] = 0.01,
+        ['Specific Weapons'] = {
+            ['Enabled'] = true,
+            ['Weapons'] = {
+                '[Double-Barrel SG]',
+                '[Revolver]',
+                '[TacticalShotgun]',
+            },
+        },
+    },
 
-			["TargetPart"] = "Head",
+    ['Spread'] = {
+        ['Enabled'] = true,
+        ['Amount'] = 1,
+        ['Specific Weapons'] = {
+            ['Enabled'] = false,
+            ['Weapons'] = {
+                '[Double-Barrel SG]',
+                '[TacticalShotgun]',
+            },
+        },
+    },
 
-			["MaxDistance"] = 300,
+    ['Speed'] = {
+        ['Enabled'] = true,
+        ['Multiplier'] = 16,
+        ['Anti Fling'] = false,
+    },
 
-			["TeamCheck"] = true
-		}
-	},
+    ['Hitbox Expander'] = {
+        ['Enabled'] = false,
+        ['Size'] = 5,
+    },
 
+    ['Spiderman'] = {
+        ['Enabled'] = false,
+    },
 
-	["FOV"] = {
-
-		["Camlock"] = {
-
-			["Enabled"] = true,
-
-			["Radius"] = 150,
-
-			["Visible"] = true
-
-		}
-	}
-
+    ['Visual Awareness'] = {
+        ['Enabled'] = true,
+        ['Color'] = Color3.fromRGB(255, 255, 255),
+        ['Target Color'] = Color3.fromRGB(255, 0, 0),
+    },
 }
-
-return shared.story
