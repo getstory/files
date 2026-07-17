094ccf444d8c96e6e7ff8daafb4a8252c9ce258c
Story <antblastinmgmt@gmail.com>
2026-07-16 21:33:19 -0400

    Add files via upload

---
+++ loaders/e123234ad434244ed2344425r42342.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/getstory/files/v1/config/config.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/getstory/files/v1/main/main.lua"))()
