580e6e4e22a3c92915b85e27ad44a7a05a87e46b
Story <antblastinmgmt@gmail.com>
2026-07-16 21:32:58 -0400

    Delete loaders directory

--- loaders/e123234ad434244ed2344425r42342.lua
+++
@@ -1,3 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/getstory/files/v1/config/config.lua"))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/getstory/files/v1/main/main.lua"))()
