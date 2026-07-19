a5a4c0af8672666101a1d5a01afda6ca8f21bb43
Story <antblastinmgmt@gmail.com>
2026-07-18 21:08:47 -0400

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,107 +1,185 @@
 shared.Story = {
-    ['Settings'] = {
-        ['Target Aim'] = true,
-        ['Knock Check'] = true,
-        ['Visible Check'] = false,
+    ['settings'] = {
+        ['knock check'] = true,
+        ['visible check'] = true,
+        ['self knock check'] = true,
+        ['knife check'] = true,
     },
-    ['Keybinds'] = {
-        ['Target Lock'] = {  -- keybind settings for camera/aim lock
-            ['Key'] = 'F',
-            ['Mode'] = 'Toggle',
+
+    ['targeting'] = {
+        ['mode'] = 'Select', -- Select/Automatic
+    },
+
+    ['binds'] = {
+        ['silent aim'] = 'C',
+        ['cam lock'] = 'X',
+        ['triggerbot'] = 'V',
+        ['super jump'] = 'B',
+        ['esp'] = 'Y',
+        ['speed'] = 'Z',
+    },
+
+    ['silent aimbot'] = {
+        ['enabled'] = true,
+        ['mode'] = 'Toggle',
+        ['hitpart'] = 'Closest Part',
+        ['use prediction'] = true,
+        ['prediction'] = {
+            ['x'] = 0.133,
+            ['y'] = 0.133,
+            ['z'] = 0.133,
         },
-        ['Trigger Bot'] = {  -- keybind settings for auto-fire when aiming at a target
-            ['Key'] = 'F',
-            ['Mode'] = 'Toggle',
+        ['fov'] = {
+            ['enabled'] = true,
+            ['visible'] = true,
+            ['mode'] = '3D', -- 3D/2D
+            ['active color'] = Color3.fromRGB(0, 17, 255),
+            ['size'] = {
+                ['x'] = 10,
+                ['y'] = 10,
+                ['z'] = 10,
+            },
         },
-        ['Speed'] = 'Q',  -- keybind to toggle movement speed boost
-        ['ESP'] = 'E',  -- keybind to toggle visual awareness/ESP display
-        ['Super Jump'] = 'V',  -- keybind to enable super jump behavior
-        ['Toggle on-screen Text'] = 'G',  -- keybind to toggle on-screen text display
-    },
-    ['FOV'] = {
-        ['Enabled'] = false,
-        ['Visible'] = true,
-        ['Size'] = Vector2.new(2000, 2000),
-        ['Thickness'] = 2,
-        ['Color'] = Color3.fromRGB(255, 255, 255),
-    },
-    ['Silent Aim'] = { -- doesnt work for der hood for some reason but works for other games
-        ['Enabled'] = true,
-        ['Hit Part'] = 'Head',
-        ['Use Prediction'] = false,
-        ['Prediction'] = {
-            ['X'] = 0,
-            ['Y'] = 0,
-            ['Z'] = 0,
+        ['distance check'] = {
+            ['enabled'] = true,
+            ['max distance'] = 300,
         },
     },
-    ['Camera Lock'] = {
-        ['Enabled'] = false,
-        ['Hit Part'] = 'Closest Part',
-        ['Smoothing'] = 40,
-        ['Use Prediction'] = true,
-        ['Prediction'] = 0.133,
+
+    ['camera aimbot'] = {
+        ['enabled'] = true,
+        ['mode'] = 'Toggle',
+        ['hitpart'] = 'Closest Part',
+        ['smoothing'] = {
+            ['x'] = 40,
+            ['y'] = 40,
+            ['z'] = 40,
+        },
+        ['use prediction'] = true,
+        ['prediction'] = {
+            ['x'] = 0.133,
+            ['y'] = 0.133,
+            ['z'] = 0.133,
+        },
+        ['fov'] = {
+            ['enabled'] = true,
+            ['visible'] = true,
+            ['mode'] = '3D', -- 3D/2D
+            ['active color'] = Color3.fromRGB(0, 17, 255),
+            ['size'] = {
+                ['x'] = 10,
+                ['y'] = 10,
+                ['z'] = 10,
+            },
+        },
+        ['distance check'] = {
+            ['enabled'] = true,
+            ['max distance'] = 300,
+        },
     },
-    ['Trigger Bot'] = {
-        ['Enabled'] = true,
-        ['Delay'] = 0.01,
-        ['Knife Check'] = true,
-        ['Specific Weapons'] = {
-            ['Enabled'] = false,
-            ['Weapons'] = {
+
+    ['trigger bot'] = {
+        ['enabled'] = true,
+        ['mode'] = 'Hold',
+        ['delay'] = 0.01,
+        ['require target'] = true,
+        ['specific weapons'] = {
+            ['enabled'] = false,
+            ['weapons'] = {
                 '[Double-Barrel SG]',
                 '[Revolver]',
                 '[TacticalShotgun]',
             },
         },
+        ['fov'] = {
+            ['enabled'] = true,
+            ['visible'] = true,
+            ['mode'] = '3D', -- 3D/2D
+            ['active color'] = Color3.fromRGB(0, 17, 255),
+            ['size'] = {
+                ['x'] = 10,
+                ['y'] = 10,
+                ['z'] = 10,
+            },
+        },
+        ['distance check'] = {
+            ['enabled'] = true,
+            ['max distance'] = 300,
+        },
     },
-    ['Spread'] = {
-        ['Enabled'] = true,
-        ['Amount'] = 26,
-        ['Specific Weapons'] = {
-            ['Enabled'] = true,
-            ['Weapons'] = {
+
+    ['target line'] = {
+        ['enabled'] = true,
+        ['thickness'] = 2.2,
+        ['transparency'] = 0.8,
+        ['vulnerable'] = Color3.fromRGB(255, 85, 127),
+        ['invulnerable'] = Color3.fromRGB(150, 150, 150),
+    },
+
+    ['spread modifications'] = {
+        ['enabled'] = true,
+        ['amount'] = 1, -- 1-100
+        ['specific weapons'] = {
+            ['enabled'] = false,
+            ['weapons'] = {
                 '[Double-Barrel SG]',
                 '[TacticalShotgun]',
             },
         },
     },
-    ['Speed'] = {
-        ['Enabled'] = true,
-        ['Multiplier'] = 35,
-        ['Anti Fling'] = false,
+
+    ['super jump'] = {
+        ['enabled'] = false,
+        ['jump power'] = 100,
     },
-    ['Hitbox Expander'] = {
-        ['Enabled'] = true,
-        ['Size'] = 3,
-    },
-    ['Spiderman'] = {
-        ['Enabled'] = false,
-    },
-    ['Visual Awareness'] = {
-        ['Enabled'] = true,
-        ['Color'] = Color3.fromRGB(255, 255, 255),
-        ['Target Color'] = Color3.fromRGB(50, 205, 50),
-    },
-    ['Super Jump'] = {
-        ['Enabled'] = true,
-        ['Power'] = 265,
-        ['Cooldown'] = 0.1,
-    },
-        ['Infinite Range'] = {
-        ['Enabled'] = true,
-        ['Key'] = 'F',
-        ['Max Range'] = 99999,
-    },
-        ['Rapid Fire'] = {
-        ['Enabled'] = true,
-        ['Delay'] = 0.02,
-        ['Specific Weapons'] = {
-            ['Enabled'] = false,
-            ['Weapons'] = {
+
+    ['rapid fire'] = {
+        ['enabled'] = false,
+        ['delay'] = 0.01,
+        ['specific weapons'] = {
+            ['enabled'] = false,
+            ['weapons'] = {
                 '[Revolver]',
-                '[Double-Barrel SG]',
             },
         },
+    },
+
+    ['hitbox expander'] = {
+        ['enabled'] = false,
+        ['size'] = 5,
+    },
+
+    ['spiderman'] = {
+        ['enabled'] = false,
+        ['jumppower'] = 50,
+        ['knifejumppower'] = 60,
+    },
+
+    ['speed modifications'] = {
+        ['enabled'] = true,
+        ['multiplier'] = 35,
+    },
+
+    ['esp'] = {
+        ['enabled'] = true,
+        ['color'] = Color3.fromRGB(255, 255, 255),
+        ['target color'] = Color3.fromRGB(255, 0, 0),
+        ['use display name'] = false,
+        ['name above'] = false,
+    },
+
+    ['skins'] = {
+        ['enabled'] = false,
+        ['weapons'] = {
+            ['[Double-Barrel SG]'] = "Golden Age",
+            ['[Revolver]'] = "Golden Age",
+            ['[TacticalShotgun]'] = "Shadow",
+            ['[Knife]'] = "Golden Age Tanto",
+        },
+    },
+
+    ['headless'] = {
+        ['enabled'] = false,
+        ['remove face accessories'] = true,
     },
 }
